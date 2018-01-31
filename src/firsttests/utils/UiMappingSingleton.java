afaab0fa10aa0713ec9ae27d84d87fcc57f1a9f0
oleksandrzinoviev <shurik.zinovyev@gmail.com>
2018-01-31 19:44:13 +0200

    UiMappingsingleton updated, propertyloader, classnameutil

--- src/firsttests/utils/UiMappingSingleton.java
+++ src/firsttests/utils/UiMappingSingleton.java
@@ -1,67 +1,108 @@
 package firsttests.utils;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
-
+/*
+ *  That class provides static methods for getting values from UIMapping.properties and uiMapping MAP
+ */
 public class UiMappingSingleton {
 
-    /// переписать класс с паттерном SingleTon
+    private volatile static UiMappingSingleton uniqueInstance;
+    private static Properties properties = new Properties();
+    private volatile static Map<String, String> uiMapping;
 
-    //TODO Maven
-    public static String uiMappingFile = "src/firsttests/utils/UIMapping.properties";
+    private static final Logger log = Logger.getLogger(ClassNameUtil.getCurrentClassName());
 
-    public static String getValueFromFile(String key, String fileName) throws IOException {
-        Properties p = new Properties();
-        try {
-            p.load(new FileInputStream(fileName));
-            // p.load
-        } catch (IOException e) {
-            throw new IllegalStateException("Incorrect file path " + e.getMessage());
+
+    private UiMappingSingleton() throws  IOException {
+        uiMapping = new HashMap<>();
+
+        long start = System.nanoTime();
+        loadDataFromUIMappingFile();
+        for (final Map.Entry<Object, Object> entry : properties.entrySet()) {
+            uiMapping.put((String) entry.getKey(), (String) entry.getValue());
         }
-        return (p.getProperty(key));
+        log.info("\t** firsttests.utils.UiMappingSingleton creation time - " + (System.nanoTime() - start) + "nanoseconds **");
     }
 
 
-    public static By ui(String key) throws IOException {
-        String[] partsOfLocators = getValueFromFile(key, uiMappingFile).split("\"");
-        String locatorType = partsOfLocators[0];
+    public static synchronized UiMappingSingleton getInstance() throws  IOException{
+        if (uiMapping == null) {
+            log.info("\t** firsttests.utils.UiMappingSingleton getInstance() class A **");
+            uniqueInstance = new UiMappingSingleton();
+            log.info("\t** Object - " + uniqueInstance + " **");
+            return uniqueInstance;
+        }
+        log.info("\t** firsttests.utils.UiMappingSingleton getInstance() class B **");
+        log.info("\t** Object - " + uniqueInstance + " **");
+        return uniqueInstance;
+    }
+
+
+    /**
+     *  This method Load locators data from uiMapping.properties which
+     *  path is specified in application.properties with key: #uiMappingFile.
+     *  Return value from .properties file
+     *
+     *  @return properties
+     */
+    public static Properties loadDataFromUIMappingFile()  throws IOException {
+        File file = new File(PropertyLoader.loadProperty("uiMappingFile"));
+
+        if (file.exists()) {
+            try {
+                properties.load(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+            } catch (IOException e) {
+                Assert.fail("Can not find UIMappingFile in class path!");
+            }
+        }
+
+        return properties;
+    }
+
+
+    /**
+     *  This method parses data from UIMapping HashMap
+     *
+     *  @return By class data type with locatorType and locatorValue
+     */
+    public static By ui(String key) {
+        String[] partsOfLocators = uiMapping.get(key).split("\"");
+        String locatorType = partsOfLocators[0].substring(0, partsOfLocators[0].length() - 1);
         String locatorValue = partsOfLocators[1];
 
-
-        // Return By class with appropriate method and locatorValue
-        // Refactor code in Switch Case
-        // save to hash map
-
-        if (locatorType.equals("id")) {
-            return By.id(locatorValue);
+        if (locatorType.equals("xpath")){
+            return By.xpath(locatorValue);
         } else {
-            if (locatorType.equals("xpath")) {
-                return By.xpath(locatorValue);
+            if (locatorType.equals("cssSelector")){
+                return By.cssSelector(locatorValue);
             } else {
-                if (locatorType.equals("name")) {
-                    return By.name(locatorValue);
+                if (locatorType.equals("id")){
+                    return By.id(locatorValue);
                 } else {
-                    if (locatorType.equals("linkText")) {
-                        return By.linkText(locatorValue);
+                    if (locatorType.equals("className")){
+                        return By.className(locatorValue);
                     } else {
-                        if (locatorType.equals("tagName")) {
-                            return By.tagName(locatorValue);
-                        } else {
-                            if (locatorType.equals("className")) {
-                                return By.className(locatorValue);
-                            } else {
-                                if (locatorType.equals("cssSelector")) {
-                                    return By.cssSelector(locatorValue);
-                                } else {
-                                    return By.partialLinkText(locatorValue);
-                                }
-                            }
-                        }
+                        return By.name(locatorValue);
                     }
                 }
             }
         }
     }
+
+    public static String uiStr(String key) {
+        String[] partsOfLocators = uiMapping.get(key).split("\"");
+        String locatorValue = partsOfLocators[1];
+
+        return locatorValue;
+    }
+
 }
---
+++ src/firsttests/utils/UiMappingSingleton_old.java
@@ -0,0 +1,67 @@
+//package firsttests.utils;
+//import org.openqa.selenium.By;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.util.Properties;
+//
+//public class UiMappingSingleton_old {
+//
+//    /// переписать класс с паттерном SingleTon
+//
+//    //TODO Maven
+//    public static String uiMappingFile = "src/firsttests/utils/UIMapping.properties";
+//
+//    public static String getValueFromFile(String key, String fileName) throws IOException {
+//        Properties p = new Properties();
+//        try {
+//            p.load(new FileInputStream(fileName));
+//            // p.load
+//        } catch (IOException e) {
+//            throw new IllegalStateException("Incorrect file path " + e.getMessage());
+//        }
+//        return (p.getProperty(key));
+//    }
+//
+//
+//    public static By ui(String key) throws IOException {
+//        String[] partsOfLocators = getValueFromFile(key, uiMappingFile).split("\"");
+//        String locatorType = partsOfLocators[0];
+//        String locatorValue = partsOfLocators[1];
+//
+//
+//        // Return By class with appropriate method and locatorValue
+//        // Refactor code in Switch Case
+//        // save to hash map
+//
+//        if (locatorType.equals("id")) {
+//            return By.id(locatorValue);
+//        } else {
+//            if (locatorType.equals("xpath")) {
+//                return By.xpath(locatorValue);
+//            } else {
+//                if (locatorType.equals("name")) {
+//                    return By.name(locatorValue);
+//                } else {
+//                    if (locatorType.equals("linkText")) {
+//                        return By.linkText(locatorValue);
+//                    } else {
+//                        if (locatorType.equals("tagName")) {
+//                            return By.tagName(locatorValue);
+//                        } else {
+//                            if (locatorType.equals("className")) {
+//                                return By.className(locatorValue);
+//                            } else {
+//                                if (locatorType.equals("cssSelector")) {
+//                                    return By.cssSelector(locatorValue);
+//                                } else {
+//                                    return By.partialLinkText(locatorValue);
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
